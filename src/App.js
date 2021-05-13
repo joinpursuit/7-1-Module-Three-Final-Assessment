63814433993d3993c8a40eb702f2cbbdc490c38f
Teyanna Earle <teyonna.earle92@gmail.com>
2021-05-13 12:10:49 -0400

    Add Assessment Answers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
+import Navbar from "./Components/Navbar"
+import Movies from "./Components/Movies"
+import People from "./Components/People"
+import Locations from "./Components/Locations"
+import { Switch, Route } from "react-router-dom"
+
 
 function App() {
   return (
     <div className="app">
+      <nav>
+        <Navbar />
+      </nav>
       <main>
-        <h1>Hello, world!</h1>
+        <Switch>
+          <Route exact path ="/">
+            <h1>Welcome to GhibliApp</h1>
+          </Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/people" component={People}></Route>
+          <Route path="/locations" component={Locations}></Route>
+        </Switch>
       </main>
     </div>
   );
---
+++ src/Components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import "./Navbar.css"
+
+function Navbar() {
+    return (
+        <>
+            <Link to ="/">
+                <img src= "https://cdn.vox-cdn.com/thumbor/s2_cd1uqU2wT_uy6vx4-GRESV8k=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19996673/Studio_Ghibli_Logo.jpg" 
+                style={{width: "50px" , height: "50px"}}
+                alt= "ghibli"
+                />
+            </Link>
+            <Link to="/movies">Movies</Link>
+            <Link to="/people">People</Link>
+            <Link to="/locations">Locations</Link>
+        </>
+    )
+}
+
+export default Navbar
