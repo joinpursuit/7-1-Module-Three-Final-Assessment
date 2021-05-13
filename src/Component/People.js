dc8524673071eee57602c935900011b3db108551
Dequisa <Dequisas@gmail.com>
2021-05-13 16:13:01 -0400

    All passing

--- src/Component/People.js
+++ src/Component/People.js
@@ -2,62 +2,67 @@ import React from 'react';
 import axios from 'axios';
 
 class People extends React.Component {
-    constructor(){
+    constructor() {
         super();
-        this.state={
-            search:"",
-            people:[],
+        this.state = {
+            search: "",
+            people: [],
             personName: '',
-            personAge:0,
-            personGender:'',
+            personAge: 0,
+            personGender: '',
             personId: '',
             isError: false,
-            dispose:''
+            dispose: '',
+          
         }
     }
 
-   personSearch = async () => {
-       const { people }=this.state
+    personSearch = async () => {
+        const { people } = this.state
         try {
-          const { data } = await axios.get(`https://ghibliapi.herokuapp.com/people/`)
-        this.setState({people:data})
-        data.map((p)=>{
-            if(p.name === this.state.search){
-                this.setState({personId:p.id
-                })} else {this.setState({dispose:p.id})}
+            const { data } = await axios.get(`https://ghibliapi.herokuapp.com/people/`)
+            this.setState({ people: data })
+            const newArray=data.filter((p)=>{
+                if(p.name === this.state.search){return p}
             })
-        const res = await axios.get(`https://ghibliapi.herokuapp.com/people/${this.state.personId}`)
-        console.log(res)
-        if(res.data.id){
-            console.log('I am an object')
-          this.setState({ personName:res.data.name, personAge:res.data.age, personGender:res.data.gender, search:'', isError: false})}
-          else{ 
-               console.log("i caught an error")
-              this.setState({isError:true})}
+            console.log(newArray)
+            if(newArray.length === 0){this.setState({personId:0})}else{
+            newArray.map((p)=>{this.setState({personId:p.id})})}
+
+            const res = await axios.get(`https://ghibliapi.herokuapp.com/people/${this.state.personId}`)
+            console.log(res)
+            if (res.data.id === this.state.personId) {
+                console.log('I am an object')
+                this.setState({ personName: res.data.name, personAge: res.data.age, personGender: res.data.gender, search: '', isError: false })
+            }
+            else {
+                console.log("i caught an error")
+                this.setState({ isError: true, personName: "" })
+            }
         } catch (e) {
             console.log("i caught an error")
-          this.setState({pokemonName:'', isError: true, search: ''})
+            this.setState({ pokemonName: '', isError: true, search: '' })
         }
-      }
+    }
 
-    handleChange = (e)=>{
+    handleChange = (e) => {
         const { value } = e.target
-        this.setState({ search: value})
+        this.setState({ search: value })
 
-      }
+    }
 
-    handleSubmit=(e)=>{
+    handleSubmit = (e) => {
         e.preventDefault()
-        this.setState({personName:''})
+        this.setState({ personName: '' })
         this.personSearch()
     }
 
 
 
     render() {
-        const { search, people, personName, personAge, personGender, isError }= this.state
+        const { search, people, personName, personAge, personGender, isError } = this.state
         console.log(personName)
-      
+
         return (
             <div>
                 <h2>Search for a Person</h2>
@@ -68,14 +73,14 @@ class People extends React.Component {
                     <button>Submit</button>
                 </form>
                 {personName ? (
-                <div>
-                <h2>Name: {personName}</h2>
-                <h2>Age: {personAge}</h2>
-                <h2>Gender: {personGender}</h2>
-                </div>
-                ): null}
+                    <div>
+                        <h2>Name: {personName}</h2>
+                        <h2>Age: {personAge}</h2>
+                        <h2>Gender: {personGender}</h2>
+                    </div>
+                ) : null}
 
-                {isError ? (<h2>Not Found</h2>):null}
+                {isError ? (<h2>Not Found</h2>) : null}
             </div>
         )
     }
