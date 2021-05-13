69153c9f0d033b6f6479253e1b52390c109b089b
Jenna Qian <jiayin1996@gmail.com>
2021-05-13 10:33:09 -0400

    Add Components & Pass Home Test

---
+++ src/Components/Locations.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Locations extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Locations
---
+++ src/Components/Movies.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Movies extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Movies
