770d5b05fa7f2f971eb578e78e1cddd3c22a5626
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-05-13 10:47:36 -0400

    Components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
+import Movies from "./components/Movies";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
+      <NavBar />
       <main>
-        <h1>Hello, world!</h1>
+        <Switch>
+          <Route exact path="/">
+        <h1>Welcome to GhibliApp</h1>
+        </Route>
+
+        <Route path="/movies" component={Movies} />
+        </Switch>
       </main>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav>
+                <Link to="/">
+                    <img src="https://www.kindpng.com/picc/m/187-1876040_popcorn-and-movie-reel-transparent-background-hd-png.png"
+                    alt="movie-img"
+                    style={{ height: "50px", width: "50px" }}
+                    />
+                </Link>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
