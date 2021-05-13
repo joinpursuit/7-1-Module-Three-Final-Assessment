8ea2e3f2c0b86470b3e0527c2f38b3b7c0525b36
Talia Pichardo <taliapichardo@gmail.com>
2021-05-13 17:28:07 -0400

    addind some css

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './NavBar.css'
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <nav>
             <NavLink to='/'>
-                <img src='https://c1.klipartz.com/pngpicture/770/979/sticker-png-movie-logo-computer-icons-cinema-film-theatre-movie-theater-play-event-tickets-thumbnail.png'
+                <img src='https://i2.wp.com/banner2.cleanpng.com/20190730/shy/kisspng-photographic-film-movie-camera-cinema-website-and-mobile-application-development-service-5d3fc924ce3b33.8538265315644613488447.jpg'
                     alt='Cinema Logo'
-                    style={{ height: '50px' }} />
+                />
             </NavLink>
             <NavLink to='/movies'>Movies</NavLink> {' '}
             <NavLink to='/people'>People</NavLink> {' '}
