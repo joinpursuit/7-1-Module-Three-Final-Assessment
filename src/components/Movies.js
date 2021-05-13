e212abf8b89cd04c83352b137179aae10bfe2a04
Jon Narine <jonnicwolf@gmail.com>
2021-05-13 12:55:53 -0400

    components set

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,10 @@ export default class Movies extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Select a Movie</h1>
+                <select name="" id="">
+                    <option value="">--</option>
+                </select>
             </div>
         )
     }
--- src/components/People.js
+++ src/components/People.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 export default class People extends Component {
+    
     render() {
         return (
             <div>
-                
+                <h1>Search for a Person</h1>
+                <input type="text" />
+                <button>Submit</button>
+                <div>Name:</div>
+                <div>Age:</div>
+                <div>Gender:</div>
             </div>
         )
     }
