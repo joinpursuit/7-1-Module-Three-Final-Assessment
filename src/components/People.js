a4894c1cab4cf0da7a97b95f59607b64367dd525
Jon Narine <jonnicwolf@gmail.com>
2021-05-13 15:38:58 -0400

    grabbing all id

--- src/components/People.js
+++ src/components/People.js
@@ -4,8 +4,8 @@ import axios from "axios";
 export default class People extends Component {
   constructor() {
     super();
-      this.state = {
-        data: [],
+    this.state = {
+      data: [],
       input: "",
       name: "",
       age: "",
@@ -18,40 +18,50 @@ export default class People extends Component {
     this.setState({
       input: e.target.value,
     });
-    console.log(this.state.input)
+    console.log(this.state.input);
   };
-//     async componentDidMount() {
-//         try {
-//             const { data } = await axios.get(
-//                 `https://ghibliapi.herokuapp.com/people`
-//             );
-//             const {name, age, gender} = data.map(person => {
-//                 console.log(person.name)
-//             })
-//         }
-//         catch(e) {
-//             console.error(e)
-//         }
-//   }
+  //     async componentDidMount() {
+  //         try {
+  //             const { data } = await axios.get(
+  //                 `https://ghibliapi.herokuapp.com/people`
+  //             );
+  //             const {name, age, gender} = data.map(person => {
+  //                 console.log(person.name)
+  //             })
+  //         }
+  //         catch(e) {
+  //             console.error(e)
+  //         }
+  //   }
   handleClick = async () => {
+    // try {
+    //   const { data } = await axios.get(
+    //     `https://ghibliapi.herokuapp.com/people`
+    //   );
+    //     data.map(person => {
+    //         //build what appears between
+    //         this.setState({
+    //           name: person.name,
+    //           age: person.age,
+    //           gender: person.gender,
+    //         });
+
+    //     })
     try {
       const { data } = await axios.get(
         `https://ghibliapi.herokuapp.com/people`
       );
-        data.map(person => {
-            //build what appears between
-            this.setState({
-              name: person.name,
-              age: person.age,
-              gender: person.gender,
-            });
-
-        })
+      const id = data.map((person) => {
+          return person.id
+          
+      });
+        console.log(id)
         
-      console.log(data);
+        
+      
     } catch (e) {
-        console.error(e);
-        console.log(this.state.name)
+      console.error(e);
+      console.log(this.state.name);
       this.setState({
         name: "",
         age: "",
@@ -65,16 +75,17 @@ export default class People extends Component {
     return (
       <div>
         <h1>Search for a Person</h1>
-            <input
-                onChange={this.handleInputChange}
-                type="text"
-                placeholder="Find your person"/>
+        <input
+          onChange={this.handleInputChange}
+          type="text"
+          placeholder="Find your person"
+        />
         <button onClick={this.handleClick}>Submit</button>
         {this.state.name ? (
           <>
-            <div> {this.state.name}</div>
+            <div> Name:{this.state.name}</div>
             <div>Age:{this.state.age}</div>
-            <div>Age:{this.state.age}</div>
+            <div>Gender:{this.state.gender}</div>
           </>
         ) : null}
       </div>
