293e9c54f17de9a773ff73eea596c08d9674a1ad
cassidybeni <cassidybeni@pursuit.org>
2021-05-13 10:41:42 -0400

    Pass tests for Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { Switch, Link, Route, } from "react-router-dom";
+import Movies from "./Movies/Movies"
+import People from "./People/People"
+import Locations from "./Locations/Locations"
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
+    <nav>
+      <Link to="/"><img style={{height: "50px", width: "50px"}} src={"https://upload.wikimedia.org/wikipedia/en/thumb/c/ca/Studio_Ghibli_logo.svg/1200px-Studio_Ghibli_logo.svg.png"} alt="studioGhibli"></img></Link> {" "}
+      <Link to="/movies" >Movies</Link> {" "}
+      <Link to="/people" >People</Link> {" "}
+      <Link to="/locations" >Locations</Link> {" "}
+    </nav>
       <main>
-        <h1>Hello, world!</h1>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Welcome to GhibliApp</h1>}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/people" component={People}></Route>
+          <Route path="/locations" component={Locations}></Route>
+        </Switch>
       </main>
     </div>
   );
