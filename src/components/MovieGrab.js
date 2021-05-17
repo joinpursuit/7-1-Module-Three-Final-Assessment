e3c036cb40e2ac5001f0536034f8aa50a1ff876f
Jon Narine <jonnicwolf@gmail.com>
2021-05-17 01:58:01 -0400

    /movies working

--- src/components/MovieGrab.js
+++ src/components/MovieGrab.js
@@ -1,52 +1,68 @@
 import React, { Component } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 export default class Movies extends Component {
-    constructor() {
-        super()
-        this.state = {
-            menu:[],
-            title: '',
-            dropDate: '',
-            description:''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      menu: [],
+      title: "",
+      dropDate: "",
+      description: "",
+    };
+  }
 
-    
-  
-  
-  
-  
-  
-  
-  
-  
   filmGrab = async () => {
-        try {
-            const { data } = await axios.get('https://ghibliapi.herokuapp.com/films')
-            this.setState({
-                menu: data
-            })
-          console.log(data)
-        }
-        catch (e) {
-            console.error(e)
-        }
+    try {
+      const { data } = await axios.get("https://ghibliapi.herokuapp.com/films");
+      this.setState({
+        menu: data,
+      });
+      console.log(data);
+    } catch (e) {
+      console.error(e);
     }
-    
-    componentDidMount() {
-        this.filmGrab()
-    }
+  };
+
+  onChange = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.get(
+      `https://ghibliapi.herokuapp.com/films/${e.target.value}`
+    );
+    console.log(data)
+    this.setState({
+      title: data.title,
+      dropDate: data.release_date,
+      description: data.description
+      
+    })
+  };
+
+  componentDidMount() {
+    this.filmGrab();
+  }
   render() {
-    const { menu } = this.state
-    const menuPop = menu.map((flick, i) => <option key={i} value={flick.id}>{flick.title }</option>)
+    const { menu } = this.state;
+    const menuPop = menu.map((flick, i) => (
+      <option key={i} value={flick.id}>
+        {flick.title}
+      </option>
+    ));
     return (
       <div>
         <h1>Select a Movie</h1>
-        <select name="" id="">
-          <option value=""></option>
+        <select
+          onChange={this.onChange}>
+          <option value="">--</option>
           {menuPop}
         </select>
+        {(this.state.title) ?
+          <>
+            <div>Title:{this.state.title}</div>
+            <div>Release Date:{this.state.dropDate}</div>
+            <div>Description:{this.state.description}</div>
+          </>
+          : null}
       </div>
     );
   }
